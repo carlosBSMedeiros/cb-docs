fd2c89fbbf6c1e21b1e6511c05465edf53b57883
carlos.medeiros <carlos.medeiros@el.com.br>
2022-03-23 23:08:35 -0300

    alterando botão principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ function HomepageHeader() {
           </Link>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/about">
             Explorar 
           </Link>
         </div>
